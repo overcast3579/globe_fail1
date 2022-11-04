3e2b7e3181aaafbfbd86499bc0a7734f1b65799d
Chris Huang <114379646+overcast3579@users.noreply.github.com>
2022-11-04 09:41:21 +0800

    scr

--- main.js
+++ main.js
@@ -1,4 +1,4 @@
-import * as THREE from '../node_modules/three/build/three.module.js'
+import * as THREE from './node_modules/three/build/three.module.js'
 const globe_container = document.querySelector('#globe_container');
 const scene = new THREE.Scene();
 const camera = new THREE.PerspectiveCamera(
